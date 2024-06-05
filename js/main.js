500f503215de9f90e747a298b25658617f184596
Néphélim <nephcohen@gmail.com>
2024-06-05 17:28:07 +0200

    Generation card

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 import '../sass/render.scss'
 import { getRecipes } from './components/api'
 import { recipesContainer } from './components/domLinker'
+import { createCard } from './factory/card'
 
 console.log(getRecipes())
 
-const span = document.createElement('span')
-span.innerHTML = 'test'
+// const span = document.createElement('span')
+// span.innerHTML = 'test'
 
-recipesContainer.appendChild(span)
+// recipesContainer.appendChild(span)
+const data = getRecipes()
+
+data.forEach(item => {
+    const card = createCard(item)
+    recipesContainer.appendChild(card)
+})
