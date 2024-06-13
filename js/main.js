0d619f2be7a0d4745ac6a37c24828001ef4a9ebd
Néphélim <nephcohen@gmail.com>
2024-06-13 13:35:42 +0200

    Maj Factory
    Change Factory System

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import '../sass/render.scss'
 import { getRecipes } from './components/api'
 import { recipesContainer } from './components/domLinker'
-import { createCard } from './factory/card'
+import { createCard } from './models/card'
 
 console.log(getRecipes())
 
@@ -15,3 +15,24 @@ data.forEach(item => {
     const card = createCard(item)
     recipesContainer.appendChild(card)
 })
+//=================================================================
+/*
+class App {
+    constructor() {
+        this.$moviesWrapper = document.querySelector('.movies-wrapper')
+        this.moviesApi = new MovieApi('/data/movie-data.json')
+    }
+
+    async main() {
+        const movies = await this.moviesApi.getMovies()
+
+        movies.forEach(movie => {
+            const Template = new MovieCard(movie)
+            this.$moviesWrapper.appendChild(Template.createMovieCard())        
+        })    
+    }
+}
+
+const petitsplats = new App()
+petitsplats.main()
+*/
