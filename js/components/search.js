7e770d0ee4abb30471218b24ac60ce6cb8505e1a
Néphélim <nephcohen@gmail.com>
2024-07-07 01:02:50 +0200

    Final algo test return !

--- js/components/search.js
+++ js/components/search.js
@@ -10,10 +10,11 @@
  * @param {String} value2
  * @returns Boolean
  */
-//export const isLowerCaseIncluded = (value1, value2) => value1.toLowerCase().includes(value2.toLowerCase())
+export const isLowerCaseIncluded = (value1, value2) => value1.toLowerCase().includes(value2.toLowerCase())
 // FOR 
-const  sortFor = (value1, value2) => value1.toLowerCase().includes(value2.toLowerCase());
+//const  sortFor = (value1, value2) => value1.toLowerCase().includes(value2.toLowerCase());
 // 
+/*
 export const isLowerCaseIncluded = (array, property, value) => {
     for (let i = 0; i < array.length; i++) {
         if (sortFor(array[i][property], value)) {
@@ -22,6 +23,7 @@ export const isLowerCaseIncluded = (array, property, value) => {
     }
     return undefined;  // Retourne undefined si aucun élément ne correspond
 }
+    */
 
 
 /**
@@ -34,9 +36,21 @@ export const isLowerCaseIncluded = (array, property, value) => {
  */
 
 //for
+/*
 export const isFound = (array, property, value) => {
-    //array.find(item => isLowerCaseIncluded(item[property], value))
-
+    array.find(item => isLowerCaseIncluded(item[property], value))
+*/
+const isFound = (array, property, value) => {
+    for (const item of array) {
+        if (isLowerCaseIncluded(item[property], value)) {
+            return true
+        }
+    }
+    return false
+}
+/* Pas necessaire ========= dans une autre vie ========= */
+/*
+export const isFound = (array, property, value) => {  
     for (let i = 0; i < array.length; i++) {
         if (sortFor(array[i][property], value)) {
             item => isLowerCaseIncluded(item[property], value)
@@ -46,6 +60,7 @@ export const isFound = (array, property, value) => {
     //
     return undefined;  // Retourne undefined si aucun élément ne correspond
 }
+*/
 /**
  * Get recipes in function of value includes in at least one of followings properties:
  * name or description or ingredients
@@ -53,10 +68,32 @@ export const isFound = (array, property, value) => {
  * @param {String} value - String to search
  * @returns Array of object of recipes
  */
+/*
 export const filterMainSearchBar = (recipes, value) => recipes.filter(item =>
     isLowerCaseIncluded(item.description, value) ||
     isLowerCaseIncluded(item.name, value) ||
     isFound(item.ingredients, 'ingredient', value))
+*/
+
+export const filterMainSearchBar = (recipes, value) => {
+    const startTime = performance.now();
+    const result = []
+    // 1st step: Loop on each recipes
+    for (const recipe of recipes) {
+        // 2nd step: Compare value with each properties (name, description, ingredients)
+        if (isLowerCaseIncluded(recipe.description, value) ||
+            isLowerCaseIncluded(recipe.name, value) ||
+            isFound(recipe.ingredients, 'ingredient', value)) {
+            // 3rd step: add recipe to a result array
+            result.push(recipe)
+        }
+    }
+    const endTime = performance.now();
+    console.log("getIngredients Execution Time:", (endTime - startTime).toFixed(10), "ms");
+    return result
+}
+
+
 
 const isRecipeIncludesEveryTagIngredient = (recipe, tags) => tags.ingredients.every(ingredient => recipe.ingredients.map(elem => elem.ingredient).includes(ingredient))
 const isRecipeIncludesEveryTagDevice = (recipe, tags) => tags.devices.every(device => recipe.appliance.includes(device))
